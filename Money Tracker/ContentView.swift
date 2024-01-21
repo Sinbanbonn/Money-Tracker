c4f0efd7a0f9a08edc8a7550f8c50d344e6a34bf
Sinbanbonn <logvinov0607@icloud.com>
2024-01-21 20:10:20 +0300

    Adding Face Id recognizer, settings and searching

--- Money Tracker/ContentView.swift
+++ Money Tracker/ContentView.swift
@@ -9,35 +9,41 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    // Intro visible status
+    // MARK: - Intro visible status
     @AppStorage("isFirstTime") private var isFirstTime: Bool = true
     
-    // Active tab
+    // MARK: - App Lock Properties
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
+    
+    // MARK: - Active tab
     @State private var activeTab: Tab = .recents
     
     var body: some View {
-        TabView(selection: $activeTab) {
-            Recents()
-                .tag(Tab.recents)
-                .tabItem { Tab.recents.tabContent }
+        LockView(lockType: .biometric, lockPin: "", isEnabled: isAppLockEnabled, lockWhenAppGoesBackground: lockWhenAppGoesBackground) {
+            TabView(selection: $activeTab) {
+                Recents()
+                    .tag(Tab.recents)
+                    .tabItem { Tab.recents.tabContent }
+                
+                Search()
+                    .tag(Tab.search)
+                    .tabItem { Tab.search.tabContent }
+                
+                Graphs()
+                    .tag(Tab.charts)
+                    .tabItem { Tab.charts.tabContent }
+                
+                Settings()
+                    .tag(Tab.settings)
+                    .tabItem { Tab.settings.tabContent }
+            }
+            .sheet(isPresented: $isFirstTime, content: {
+                InfoScreen()
+                    .interactiveDismissDisabled()
+            })
             
-            Search()
-                .tag(Tab.search)
-                .tabItem { Tab.search.tabContent }
-            
-            Graphs()
-                .tag(Tab.charts)
-                .tabItem { Tab.charts.tabContent }
-            
-            Settings()
-                .tag(Tab.settings)
-                .tabItem { Tab.settings.tabContent }
         }
-        .sheet(isPresented: $isFirstTime, content: {
-            InfoScreen()
-                .interactiveDismissDisabled()
-        })
-        
     }
     
 }
--- Money Tracker/View/TabViews/Settings.swift
+++ Money Tracker/View/TabViews/Settings.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct Settings: View {
+    // MARK: - User Properties
+    @AppStorage("userName") private var userName: String = ""
+    
+    // MARK: - App Lock Properties
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("User Name") {
+                    TextField("iJustine", text: $userName)
+                }
+                
+                Section("App Lock") {
+                    Toggle("Enable App Lock", isOn: $isAppLockEnabled)
+                    
+                    if isAppLockEnabled {
+                        Toggle("Lock when app goes down", isOn: $lockWhenAppGoesBackground)
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
